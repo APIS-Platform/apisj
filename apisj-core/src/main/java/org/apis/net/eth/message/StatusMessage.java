d8f96d0accbb869558de12ae136aecff438f950a
Daniel <danieluckee@gmail.com>
2018-06-19 20:13:41 +0900

    Added coinbase value in StatusMessage.

--- apisj-core/src/main/java/org/apis/net/eth/message/StatusMessage.java
+++ apisj-core/src/main/java/org/apis/net/eth/message/StatusMessage.java
@@ -48,17 +48,20 @@ public class StatusMessage extends EthMessage {
      */
     private byte[] genesisHash;
 
+    private byte[] coinbase;
+
     public StatusMessage(byte[] encoded) {
         super(encoded);
     }
 
     public StatusMessage(byte protocolVersion, int networkId,
-                         byte[] totalRewardPoint, byte[] bestHash, byte[] genesisHash) {
+                         byte[] totalRewardPoint, byte[] bestHash, byte[] genesisHash, byte[] coinbase) {
         this.protocolVersion = protocolVersion;
         this.networkId = networkId;
         this.totalRewardPoint = totalRewardPoint;
         this.bestHash = bestHash;
         this.genesisHash = genesisHash;
+        this.coinbase = coinbase;
         this.parsed = true;
     }
 
@@ -74,6 +77,7 @@ public class StatusMessage extends EthMessage {
         this.totalRewardPoint = (rewardPoint == null) ? ByteUtil.ZERO_BYTE_ARRAY : rewardPoint;
         this.bestHash = paramsList.get(3).getRLPData();
         this.genesisHash = paramsList.get(4).getRLPData();
+        this.coinbase = paramsList.get(5).getRLPData();
 
         parsed = true;
     }
@@ -84,9 +88,10 @@ public class StatusMessage extends EthMessage {
         byte[] totalDifficulty = RLP.encodeElement(this.totalRewardPoint);
         byte[] bestHash = RLP.encodeElement(this.bestHash);
         byte[] genesisHash = RLP.encodeElement(this.genesisHash);
+        byte[] coinbase = RLP.encodeElement(this.coinbase);
 
         this.encoded = RLP.encodeList( protocolVersion, networkId,
-                totalDifficulty, bestHash, genesisHash);
+                totalDifficulty, bestHash, genesisHash, coinbase);
     }
 
     @Override
@@ -129,6 +134,11 @@ public class StatusMessage extends EthMessage {
         return genesisHash;
     }
 
+    public byte[] getCoinbase() {
+        parse();
+        return coinbase;
+    }
+
     @Override
     public EthMessageCodes getCommand() {
         return EthMessageCodes.STATUS;
@@ -138,12 +148,14 @@ public class StatusMessage extends EthMessage {
     @Override
     public String toString() {
         parse();
+        String coinbase = Hex.toHexString(this.genesisHash);
         return "[" + this.getCommand().name() +
                 " protocolVersion=" + this.protocolVersion +
                 " networkId=" + this.networkId +
                 " totalRewardPoint=" + ByteUtil.toHexString(this.totalRewardPoint) +
                 " bestHash=" + Hex.toHexString(this.bestHash) +
                 " genesisHash=" + Hex.toHexString(this.genesisHash) +
+                " coinbase=" + coinbase.substring(0, 3) + ".." + coinbase.substring(coinbase.length() - 2, coinbase.length()) +
                 "]";
     }
 }
