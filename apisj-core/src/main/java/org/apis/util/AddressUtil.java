ca922f0afaef35696c013fbe1483d7eb012911c0
Daniel <danieluckee@gmail.com>
2018-06-19 20:06:36 +0900

    Added a utility to display a short String as an address of type byte [].

---
+++ apisj-core/src/main/java/org/apis/util/AddressUtil.java
@@ -0,0 +1,10 @@
+package org.apis.util;
+
+import org.spongycastle.util.encoders.Hex;
+
+public class AddressUtil {
+    public static String getShortAddress(byte[] address, int length) {
+        String addr = Hex.toHexString(address);
+        return addr.substring(0, length) + ".." + addr.substring(addr.length() - length, addr.length());
+    }
+}
