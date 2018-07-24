e82d27cab45119c3c83637e09e7afaa159a047e8
DMK <dmkim@apisplatform.io>
2018-07-24 21:00:22 +0900

    JavaFx Application GUI Add - Smart Contract Tab Read and Write Add

--- apisj-core/src/main/java/org/apis/gui/controller/SmartContractController.java
+++ apisj-core/src/main/java/org/apis/gui/controller/SmartContractController.java
@@ -1,9 +1,13 @@
 package org.apis.gui.controller;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
-import javafx.scene.control.Label;
+import javafx.scene.control.*;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.InputEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.GridPane;
@@ -25,7 +29,17 @@ public class SmartContractController implements Initializable {
     @FXML
     private AnchorPane tab1, tab2, tab3, tab4, sideTab1, sideTab2, tab1LeftPane, tab1RightPane, tab2LeftPane;
     @FXML
+    private AnchorPane tab1AmountPane, tab2AmountPane, tab2ReadWritePane;
+    @FXML
     private GridPane transferBtn;
+    @FXML
+    private Label cSelectHeadText, cSelectItemDefaultText, cSelectItemBalanceText;
+    @FXML
+    private ImageView cSelectHeadImg;
+    @FXML
+    private VBox cSelectList, cSelectChild;
+    @FXML
+    private GridPane cSelectHead, cSelectItemDefault, cSelectItemBalance;
     @FXML
     private VBox pSelectList, pSelectChild;
     @FXML
@@ -34,7 +48,10 @@ public class SmartContractController implements Initializable {
     private VBox pSelectList_1, pSelectChild_1;
     @FXML
     private GridPane pSelectHead_1, pSelectItem100_1, pSelectItem75_1, pSelectItem50_1, pSelectItem25_1, pSelectItem10_1;
+    @FXML
+    private TextField tab1AmountTextField, tab2AmountTextField, tab1GasLimitTextField, tab2GasLimitTextField, tab2ReadWriteTextField1, tab2ReadWriteTextField2;
 
+    private Image downGrey, downWhite;
     private ArrayList<VBox> pSelectLists = new ArrayList<>();
     private ArrayList<VBox> pSelectChildList = new ArrayList<>();
     private ArrayList<GridPane> pSelectHeadList = new ArrayList<>();
@@ -59,6 +76,11 @@ public class SmartContractController implements Initializable {
         this.sideTabLabel1.setStyle("-fx-font-family: 'Open Sans SemiBold'; -fx-font-size:12px;");
         this.sideTabLinePane1.setVisible(true);
 
+        // Image Setting
+        downGrey = new Image("image/ic_down_gray@2x.png");
+        downWhite = new Image("image/ic_down_white@2x.png");
+
+        // Percentage Select Box List Handling
         pSelectLists.add(pSelectList);
         pSelectChildList.add(pSelectChild);
         pSelectHeadList.add(pSelectHead);
@@ -80,6 +102,73 @@ public class SmartContractController implements Initializable {
         hidePercentSelectBox(0);
         hidePercentSelectBox(1);
 
+        // Contract Read and Write Select Box List Handling
+        initContract();
+        hideContractSelectBox();
+
+        tab1AmountTextField.focusedProperty().addListener(tab1AmountListener);
+        tab1GasLimitTextField.focusedProperty().addListener(tab1GasLimitListener);
+        tab2AmountTextField.focusedProperty().addListener(tab2AmountListener);
+        tab2GasLimitTextField.focusedProperty().addListener(tab2GasLimitListener);
+
+    }
+
+    private ChangeListener<Boolean> tab1AmountListener = new ChangeListener() {
+        @Override
+        public void changed(ObservableValue observable, Object oldValue, Object newValue) {
+            textFieldFocus();
+        }
+    };
+
+    private ChangeListener<Boolean> tab1GasLimitListener = new ChangeListener() {
+        @Override
+        public void changed(ObservableValue observable, Object oldValue, Object newValue) {
+            textFieldFocus();
+        }
+    };
+
+    private ChangeListener<Boolean> tab2AmountListener = new ChangeListener() {
+        @Override
+        public void changed(ObservableValue observable, Object oldValue, Object newValue) {
+            textFieldFocus();
+        }
+    };
+
+    private ChangeListener<Boolean> tab2GasLimitListener = new ChangeListener() {
+        @Override
+        public void changed(ObservableValue observable, Object oldValue, Object newValue) {
+            textFieldFocus();
+        }
+    };
+
+    public void textFieldFocus() {
+        if(tab1AmountTextField.isFocused()) {
+            tab1AmountPane.setStyle("-fx-background-color: #ffffff; -fx-border-color: #999999; -fx-border-radius : 4 4 4 4; -fx-background-radius: 4 4 4 4;");
+        } else {
+            tab1AmountPane.setStyle("-fx-background-color: #f2f2f2; -fx-border-color: #d8d8d8; -fx-border-radius : 4 4 4 4; -fx-background-radius: 4 4 4 4;");
+        }
+
+        if(tab1GasLimitTextField.isFocused()) {
+            tab1GasLimitTextField.setStyle("-fx-background-color: #ffffff; -fx-border-color: #999999; -fx-border-radius : 4 4 4 4; -fx-background-radius: 4 4 4 4;" +
+                    " -fx-font-family: 'Open Sans SemiBold'; -fx-font-size:12px;");
+        } else {
+            tab1GasLimitTextField.setStyle("-fx-background-color: #f2f2f2; -fx-border-color: #d8d8d8; -fx-border-radius : 4 4 4 4; -fx-background-radius: 4 4 4 4;" +
+                    " -fx-font-family: 'Open Sans SemiBold'; -fx-font-size:12px;");
+        }
+
+        if(tab2AmountTextField.isFocused()) {
+            tab2AmountPane.setStyle("-fx-background-color: #ffffff; -fx-border-color: #999999; -fx-border-radius : 4 4 4 4; -fx-background-radius: 4 4 4 4;");
+        } else {
+            tab2AmountPane.setStyle("-fx-background-color: #f2f2f2; -fx-border-color: #d8d8d8; -fx-border-radius : 4 4 4 4; -fx-background-radius: 4 4 4 4;");
+        }
+
+        if(tab2GasLimitTextField.isFocused()) {
+            tab2GasLimitTextField.setStyle("-fx-background-color: #ffffff; -fx-border-color: #999999; -fx-border-radius : 4 4 4 4; -fx-background-radius: 4 4 4 4;" +
+                    " -fx-font-family: 'Open Sans SemiBold'; -fx-font-size:12px;");
+        } else {
+            tab2GasLimitTextField.setStyle("-fx-background-color: #f2f2f2; -fx-border-color: #d8d8d8; -fx-border-radius : 4 4 4 4; -fx-background-radius: 4 4 4 4;" +
+                    " -fx-font-family: 'Open Sans SemiBold'; -fx-font-size:12px;");
+        }
     }
 
     public void contractReadWritePopup() {
@@ -110,28 +199,54 @@ public class SmartContractController implements Initializable {
 
         }
 
+        // Amount Percentage Select Box
         String tempId = null;
         for(int i=0; i<pSelectHeadList.size(); i++){
             if(i == 0) {
                 tempId = "pSelectHead";
-            }else {
+            } else {
                 tempId = "pSelectHead_"+i;
             }
 
             if(fxid.equals(tempId)){
-                if(this.pSelectLists.get(i).isVisible() == true){
+                if(this.pSelectLists.get(i).isVisible() == true) {
                     hidePercentSelectBox(i);
-                }else{
+                } else {
                     showPercentSelectBox(i);
                 }
             }
         }
+
+        // Contract Read and Write Select Box
+        if(fxid.equals("cSelectHead")) {
+            if(this.cSelectList.isVisible() == true) {
+                hideContractSelectBox();
+                System.out.println("cSelectHead hide");
+            } else {
+                showContractSelectBox();
+                System.out.println("cSelectHead show");
+            }
+        } else if(fxid.equals("cSelectItemDefault")) {
+            initContract();
+            hideContractSelectBox();
+        } else if(fxid.equals("cSelectItemBalance")) {
+            cSelectHead.setStyle("-fx-background-color: #f2f2f2; -fx-border-color: d8d8d8; -fx-border-radius : 4 4 4 4; -fx-background-radius: 4 4 4 4;");
+            cSelectHeadText.setText(cSelectItemBalanceText.getText());
+            cSelectHeadText.setTextFill(Color.web("#999999"));
+            cSelectHeadImg.setImage(downGrey);
+            tab2ReadWritePane.setVisible(true);
+            tab2ReadWritePane.prefHeightProperty().setValue(-1);
+
+            hideContractSelectBox();
+        }
+
     }
 
     @FXML
     private void onMouseEntered(InputEvent event){
         String id = ((Node)event.getSource()).getId();
 
+        // Amount Percentage Select Box
         for(int i=0; i<pSelectHeadList.size(); i++){
             if(id.equals((i == 0) ? "pSelectItem100" : "pSelectItem100_"+i)){
                 pSelectItem100List.get(i).setStyle("-fx-background-color : #f2f2f2");
@@ -150,12 +265,21 @@ public class SmartContractController implements Initializable {
 
             }
         }
+
+        // Contract Read and Write Select Box
+        if(id.equals("cSelectItemDefault")) {
+            cSelectItemDefault.setStyle("-fx-background-color: #f2f2f2;");
+        } else if(id.equals("cSelectItemBalance")) {
+            cSelectItemBalance.setStyle("-fx-background-color: #f2f2f2;");
+        }
+
     }
 
     @FXML
     private void onMouseExited(InputEvent event){
         String id = ((Node)event.getSource()).getId();
 
+        // Amount Percentage Select Box
         for(int i=0; i<pSelectHeadList.size(); i++){
             if(id.equals((i == 0) ? "pSelectItem100" : "pSelectItem100_"+i)){
                 pSelectItem100List.get(i).setStyle("-fx-background-color : #ffffff");
@@ -174,6 +298,14 @@ public class SmartContractController implements Initializable {
 
             }
         }
+
+        // Contract Read and Write Select Box
+        if(id.equals("cSelectItemDefault")) {
+            cSelectItemDefault.setStyle("-fx-background-color: #ffffff;");
+        } else if(id.equals("cSelectItemBalance")) {
+            cSelectItemBalance.setStyle("-fx-background-color: #ffffff;");
+        }
+
     }
 
     public void showPercentSelectBox(int index){
@@ -186,6 +318,20 @@ public class SmartContractController implements Initializable {
         this.pSelectLists.get(index).setVisible(false);
         this.pSelectLists.get(index).prefHeightProperty().setValue(0);
         this.pSelectChildList.get(index).prefHeightProperty().setValue(48);
+    }
+
+    public void showContractSelectBox(){
+        this.cSelectList.setVisible(true);
+        this.cSelectList.prefHeightProperty().setValue(-1);
+        this.cSelectChild.prefHeightProperty().setValue(-1);
+        System.out.println("done");
+    }
+
+    public void hideContractSelectBox(){
+        this.cSelectList.setVisible(false);
+        this.cSelectList.prefHeightProperty().setValue(0);
+        this.cSelectChild.prefHeightProperty().setValue(40);
+        System.out.println("complete");
     }
 
     public void initTab(int index) {
@@ -230,13 +376,11 @@ public class SmartContractController implements Initializable {
             this.sideTabLabel1.setTextFill(Color.web("#910000"));
             this.sideTabLabel1.setStyle("-fx-font-family: 'Open Sans SemiBold'; -fx-font-size:12px;");
             this.sideTabLinePane1.setVisible(true);
-            System.out.println("111111111");
 
         } else if(index == 1) {
             this.sideTabLabel2.setTextFill(Color.web("#910000"));
             this.sideTabLabel2.setStyle("-fx-font-family: 'Open Sans SemiBold'; -fx-font-size:12px;");
             this.sideTabLinePane2.setVisible(true);
-            System.out.println("222222222");
 
         }
     }
@@ -268,5 +412,15 @@ public class SmartContractController implements Initializable {
         sideTabLinePane1.setVisible(false);
         sideTabLinePane2.setVisible(false);
     }
+
+    public void initContract() {
+        cSelectHead.setStyle("-fx-background-color: #999999; -fx-border-color: d8d8d8; -fx-border-radius : 4 4 4 4; -fx-background-radius: 4 4 4 4;");
+        cSelectHeadText.setText(cSelectItemDefaultText.getText());
+        cSelectHeadText.setTextFill(Color.web("#ffffff"));
+        cSelectHeadImg.setImage(downWhite);
+        tab2ReadWritePane.setVisible(false);
+        tab2ReadWritePane.prefHeightProperty().setValue(0);
+    }
+
 
 }
