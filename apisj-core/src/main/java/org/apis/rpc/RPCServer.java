f5ff203feb652a3fae1e64cc9f8bbc14cd99d914
jhkim1 <jhkim@apisplatform.io>
2018-07-24 17:22:53 +0900

    rpc server - divide hash - command blockinfo (use blockdata) - change system log

--- apisj-core/src/main/java/org/apis/rpc/RPCServer.java
+++ apisj-core/src/main/java/org/apis/rpc/RPCServer.java
@@ -72,7 +72,7 @@ public class RPCServer extends WebSocketServer {
 
     @Override
     public void onOpen(WebSocket conn, ClientHandshake handshake) {
-        System.out.println("=========== connected client : " + conn.getLocalSocketAddress() + " ===========");
+        ConsoleUtil.printBlue("=========== connected client : " + conn.getLocalSocketAddress() + " ===========\n");
 
         connectionTimeoutTimer = new Timer();
         connectionTimeoutTimerTask = new TimerTask() {
@@ -90,15 +90,21 @@ public class RPCServer extends WebSocketServer {
     @Override
     public void onClose(WebSocket conn, int code, String reason, boolean remote) {
         String host = conn.getRemoteSocketAddress().getHostName();
+        System.out.println("close host:" + host);
+
+        if (userMap.get(host) == null) {
+            System.out.println("null");
+            return;
+        }
         if (userMap.get(host).getWebSocket() == conn) {
             // 서버 등록 삭제
-            System.out.println("unregist Client");
+            ConsoleUtil.printBlue("unregist Client\n");
             userMap.remove(host);
         }
 
 
         broadcast( conn + " has left the room!" );
-        System.out.println( conn + " has left the room!" );
+        ConsoleUtil.printBlue( conn + " has left the room!\n" );
     }
 
     @Override
@@ -138,7 +144,7 @@ public class RPCServer extends WebSocketServer {
                 sAuth = JsonUtil.createAuth(salt, tempID, tempPassword.toCharArray());
 
                 if (tAuth.equals(sAuth)) {
-                    System.out.println("============ pass ====================");
+                    ConsoleUtil.printBlue("============ pass ====================\n");
                     cancelTimeout();
 
                     // create client(token) & register
@@ -159,7 +165,7 @@ public class RPCServer extends WebSocketServer {
                 }
 
                 else {
-                    System.out.println("============ non pass ====================");
+                    ConsoleUtil.printBlue("============ non pass ====================\n");
                 }
             } catch (ParseException e) {
                 e.printStackTrace();
@@ -226,7 +232,7 @@ public class RPCServer extends WebSocketServer {
 
     @Override
     public void onStart() {
-        System.out.println("Server started!");
+        ConsoleUtil.printBlue("Server started!\n");
     }
 
 
