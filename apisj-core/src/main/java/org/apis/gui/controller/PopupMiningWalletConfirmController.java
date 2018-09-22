5000226c5a462a312ac0dc98e06cd805699de3bd
wagi <waguwagi@naver.com>
2018-09-22 18:22:11 +0900

    일시제거 - SmartContract Canvas

--- apisj-core/src/main/java/org/apis/gui/controller/PopupMiningWalletConfirmController.java
+++ apisj-core/src/main/java/org/apis/gui/controller/PopupMiningWalletConfirmController.java
@@ -1,14 +1,19 @@
 package org.apis.gui.controller;
 
+import com.google.zxing.WriterException;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.InputEvent;
+import javafx.scene.shape.Rectangle;
+import org.apis.gui.common.IdenticonGenerator;
 import org.apis.gui.manager.AppManager;
 import org.apis.gui.manager.StringManager;
 import org.apis.gui.model.WalletItemModel;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -16,12 +21,9 @@ public class PopupMiningWalletConfirmController implements Initializable {
     private WalletItemModel itemModel;
     private boolean isChangable = false;
 
-    @FXML
-    private Label address, startBtn;
-    @FXML
-    private ApisTextFieldController passwordFieldController;
-    @FXML
-    private Label title, subTitle, passwordLabel, addressLabel,addressComment;
+    @FXML private Label title, subTitle, passwordLabel, addressLabel,addressComment, address, startBtn;
+    @FXML private ImageView addressIcon;
+    @FXML private ApisTextFieldController passwordFieldController;
 
     public void exit(){
         AppManager.getInstance().guiFx.hideMainPopup(0);
@@ -57,6 +59,13 @@ public class PopupMiningWalletConfirmController implements Initializable {
                 }
             }
         });
+
+        // set a clip to apply rounded border to the original image.
+        // set a clip to apply rounded border to the original image.
+        Rectangle clip = new Rectangle( this.addressIcon.getFitWidth()-0.5, this.addressIcon.getFitHeight()-0.5 );
+        clip.setArcWidth(30);
+        clip.setArcHeight(30);
+        addressIcon.setClip(clip);
     }
     public void languageSetting() {
         title.textProperty().bind(StringManager.getInstance().popup.miningWalletConfirmTitle);
@@ -81,5 +90,13 @@ public class PopupMiningWalletConfirmController implements Initializable {
     public void setModel(WalletItemModel model) {
         this.itemModel = model;
         address.textProperty().setValue(this.itemModel.getAddress());
+
+        try {
+            addressIcon.setImage(IdenticonGenerator.generateIdenticonsToImage(this.itemModel.getAddress(), 128,128));
+        } catch (WriterException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
