7f156f16f56cf661571127f978cbfb58614a740a
danieluckee@gmail.com <qkddnfQhthd2>
2018-11-05 22:23:15 +0900

    GUI에서 컨트렉트를 배포할 때 public constructor가 없을 경우 정상적으로 컨트렉트가 생성되지 않는 현상이 있어서 수정하였음

--- apisj-core/src/main/resources/contract/Wink.sol
+++ apisj-core/src/main/resources/contract/Wink.sol
@@ -5,8 +5,13 @@ contract WinkTest {
      * @dev 컨트렉트에서 수수료를 부담하도록 하기 위한 이벤트
      */
     event Wink (address beneficiary, address winker);
+    event Text (address beneficiary, address winker);
 
     uint256 foo;
+
+    constructor ()
+    public
+    {}
 
     function test()
     public
@@ -20,5 +25,7 @@ contract WinkTest {
     public
     {
         foo += 1;
+
+        emit Text(msg.sender, address(this));
     }
 }
