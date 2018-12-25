35e08c91a3094d2288d64e48220f35db2ac2bcac
wagi <waguwagi@naver.com>
2018-12-25 11:27:48 +0900

    수정사항처리 - 로딩화면 대기 1초로 변경

--- apisj-core/src/main/java/org/apis/gui/run/MainFX.java
+++ apisj-core/src/main/java/org/apis/gui/run/MainFX.java
@@ -66,7 +66,7 @@ public class MainFX extends Application  {
         public void run() {
 
             try {
-                Thread.sleep(3000);
+                Thread.sleep(1000);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
