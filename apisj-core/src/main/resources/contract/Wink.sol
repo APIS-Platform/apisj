ad15ca534aad076a7899f32954ddcd7ad56b4abd
Daniel <danieluckee@gmail.com>
2018-12-26 09:52:25 +0900

    Wink 이벤트 판별 시, winker 항목을 제거하였음. LogInfo  내의 address 속성으로 이벤트를 발생시킨 컨트렉트를 구분할 수 있으므로

--- apisj-core/src/main/resources/contract/Wink.sol
+++ apisj-core/src/main/resources/contract/Wink.sol
@@ -4,8 +4,8 @@ contract WinkTest {
     /**
      * @dev 컨트렉트에서 수수료를 부담하도록 하기 위한 이벤트
      */
-    event Wink (address beneficiary, address winker);
-    event Text (address beneficiary, address winker);
+    event Wink (address beneficiary);
+    event Text (address beneficiary);
 
     uint256 foo;
 
