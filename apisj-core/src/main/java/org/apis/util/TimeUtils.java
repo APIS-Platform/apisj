06a9f200baebf2e21080a6fe15b7f2091221cdae
Daniel <danieluckee@gmail.com>
2018-12-29 00:23:05 +0900

    시간을 서버에서 불러올 때 로그가 출력되던걸 삭제하였음

--- apisj-core/src/main/java/org/apis/util/TimeUtils.java
+++ apisj-core/src/main/java/org/apis/util/TimeUtils.java
@@ -1,20 +1,3 @@
-/*
- * Copyright (c) [2016] [ <ether.camp> ]
- * This file is part of the ethereumJ library.
- *
- * The ethereumJ library is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * The ethereumJ library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with the ethereumJ library. If not, see <http://www.gnu.org/licenses/>.
- */
 package org.apis.util;
 
 import org.apache.commons.net.ntp.NTPUDPClient;
@@ -24,8 +7,6 @@ import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.net.InetAddress;
-import java.net.SocketTimeoutException;
-import java.net.UnknownHostException;
 
 /**
  * @author Mikhail Kalinin
@@ -112,7 +93,6 @@ public class TimeUtils {
             timeClient.setDefaultTimeout(5000);
 
             String serverName = getTimeServerName();
-            System.err.print(serverName);
             InetAddress inetAddress = InetAddress.getByName(serverName);
             TimeInfo timeInfo = timeClient.getTime(inetAddress);
             timeDiff = timeInfo.getMessage().getTransmitTimeStamp().getTime() - System.currentTimeMillis();
