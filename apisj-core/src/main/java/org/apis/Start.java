15362b4849d1fb73d9c6102668712eb839526d4e
Daniel <danieluckee@gmail.com>
2019-01-22 13:08:08 +0900

    Start.java 파일에 프로그램 재시작 내용 추가하였음

--- apisj-core/src/main/java/org/apis/Start.java
+++ apisj-core/src/main/java/org/apis/Start.java
@@ -12,12 +12,15 @@ import org.apis.listener.EthereumListener;
 import org.apis.listener.EthereumListenerAdapter;
 import org.apis.rpc.RPCServerManager;
 import org.apis.util.ConsoleUtil;
+import org.apis.util.TimeUtils;
 import org.apis.util.blockchain.ApisUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 
 public class Start {
@@ -27,6 +30,24 @@ public class Start {
     private static boolean isRunRpc = false;
     private static boolean synced = false;
     protected static Logger logger = LoggerFactory.getLogger("start");
+    /**
+     * 마지막으로 블록을 전달 받은 이후, 일정 시간이 지나도 싱크가 안된다면 프로그램을 재시작하고자 한다.
+     * 이를 위해 마지막으로 블록을 전달받은 시간을 저장한다.
+     */
+    private static long timeLastBlockReceived = 0;
+
+    private static long timeLastProgramClosed = 0;
+
+    /**
+     * 블록 싱크가 멈춘 후, 프로그램을 재시작하기까지 대기하는 시간
+     */
+    private static final long TIME_CLOSE_WAIT = 3*60*1_000L;
+
+    private static final long TIME_RESTART_WAIT = 10*1_000L;
+
+    private static boolean isClosed = false;
+
+
 
     public static void main(String args[]) throws IOException {
         new CLIStart();
@@ -46,9 +67,7 @@ public class Start {
             config.setDiscoveryEnabled(false);
         }
 
-        mApis = ApisFactory.createEthereum();
-        mApis.addListener(mListener);
-        mApis.getBlockMiner().setMinGasPrice(ApisUtil.convert(50, ApisUtil.Unit.nAPIS));
+        startAPIS();
 
         if (actionBlocksLoader) {
             mApis.getBlockLoader().loadBlocks();
@@ -67,17 +86,44 @@ public class Start {
         }
     }
 
+    private static void startAPIS() {
+        mApis = ApisFactory.createEthereum();
+        mApis.addListener(mListener);
+        mApis.getBlockMiner().setMinGasPrice(ApisUtil.convert(50, ApisUtil.Unit.nAPIS));
+    }
+
     private static EthereumListener mListener = new EthereumListenerAdapter() {
 
         @Override
         public void onSyncDone(SyncState state) {
             synced = true;
             logger.debug(ConsoleUtil.colorBRed("\nSYNC DONE =============================================="));
+
+            /*
+             * 싱크가 완료된 이후, 현재의 싱크 상태를 확인해서 프로그램 재시작 여부를 판단한다.
+             */
+            Executors.newSingleThreadScheduledExecutor().scheduleAtFixedRate(() -> {
+                long now = TimeUtils.getRealTimestamp();
+
+                // 싱크가 지연된 경우 프로그램을 종료한다.
+                if(!isClosed && now - timeLastBlockReceived >= TIME_CLOSE_WAIT) {
+                    timeLastProgramClosed = now;
+                    isClosed = true;
+                    mApis.close();
+                }
+
+                // 프로그램 종료 후 일정 시간이 경과하면 프로그램을 시작시킨다.
+                if(isClosed && timeLastProgramClosed > 0 && now - timeLastProgramClosed >= TIME_RESTART_WAIT) {
+                    startAPIS();
+                }
+
+            }, 30, 1, TimeUnit.SECONDS);
         }
 
         @Override
         public void onBlock(Block block, List<TransactionReceipt> receipts) {
             logger.debug(ConsoleUtil.colorBBlue("\nOnBlock : %s (%.2f kB)", block.getShortDescr(), block.getEncoded().length/1000f));
+            timeLastBlockReceived = TimeUtils.getRealTimestamp();
 
             // 체인 싱크가 완료되면 SQL 서버 싱크를 시작한다.
             if(synced && isRunRpc) {
@@ -86,4 +132,6 @@ public class Start {
             }
         }
     };
+
+
 }
