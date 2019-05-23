c8732a5792ac65f75ebfe5524c57566fcb669309
DMK <dmkim@apisplatform.io>
2019-05-23 17:02:49 +0900

    Modify peers popup table gui
    Peer table view convenience has been improved.
    Peer popup gui updated.

--- apisj-core/src/main/java/org/apis/gui/controller/popup/PopupPeersController.java
+++ apisj-core/src/main/java/org/apis/gui/controller/popup/PopupPeersController.java
@@ -1,34 +1,37 @@
 package org.apis.gui.controller.popup;
 
-import com.sun.javafx.scene.control.skin.TableColumnHeader;
 import com.sun.javafx.scene.control.skin.TableHeaderRow;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.collections.transformation.FilteredList;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
 import javafx.util.Callback;
+import org.apis.gui.controller.base.BaseFxmlController;
 import org.apis.gui.controller.base.BasePopupController;
 import org.apis.gui.manager.StringManager;
 import org.apis.gui.model.PeersModel;
 
+import java.io.IOException;
 import java.net.URL;
-import java.util.ArrayList;
 import java.util.Comparator;
-import java.util.List;
 import java.util.ResourceBundle;
 
 import static javafx.scene.control.TableColumn.SortType.ASCENDING;
 
 public class PopupPeersController extends BasePopupController {
     @FXML private GridPane defaultGrid, detailGrid;
-    @FXML private Label titleLabel, subTitleLabel, guideLabel;
+    @FXML private VBox peersDetailVBox;
+    @FXML private Label titleLabel, subTitleLabel, guideLabel, selectedNode, viaNode;
     @FXML private TableView peersTable;
     @FXML private TableColumn<PeersModel, Integer> nodeIdCol;
     @FXML private TableColumn<PeersModel, String> nodeServiceCol, userAgentCol, pingCol;
@@ -65,6 +68,10 @@ public class PopupPeersController extends BasePopupController {
                 });
             }
         });
+
+        // Add select table contents listener
+        peersTable.setOnMouseClicked(mouseEventHandler);
+        setDefaultVisible();
     }
 
     public void languageSetting() {
@@ -85,6 +92,26 @@ public class PopupPeersController extends BasePopupController {
         peersData.add(new PeersModel(2, "192.168.0.2:8080", "/Apis Core:0.8.810/", "65548 ms"));
         peersData.add(new PeersModel(3, "192.168.0.5:8080", "/Apis Core:0.8.810/", "102 ms"));
         peersData.add(new PeersModel(4, "192.168.0.6:8080", "/Apis Core:0.8.810/", "250 ms"));
+        peersData.add(new PeersModel());
+        peersData.add(new PeersModel(1, "192.168.0.3:8080", "/Apis Core:0.8.810/", "12 ms"));
+        peersData.add(new PeersModel(2, "192.168.0.2:8080", "/Apis Core:0.8.810/", "65548 ms"));
+        peersData.add(new PeersModel(3, "192.168.0.5:8080", "/Apis Core:0.8.810/", "102 ms"));
+        peersData.add(new PeersModel(4, "192.168.0.6:8080", "/Apis Core:0.8.810/", "250 ms"));
+        peersData.add(new PeersModel());
+        peersData.add(new PeersModel(1, "192.168.0.3:8080", "/Apis Core:0.8.810/", "12 ms"));
+        peersData.add(new PeersModel(2, "192.168.0.2:8080", "/Apis Core:0.8.810/", "65548 ms"));
+        peersData.add(new PeersModel(3, "192.168.0.5:8080", "/Apis Core:0.8.810/", "102 ms"));
+        peersData.add(new PeersModel(4, "192.168.0.6:8080", "/Apis Core:0.8.810/", "250 ms"));
+        peersData.add(new PeersModel());
+        peersData.add(new PeersModel(1, "192.168.0.3:8080", "/Apis Core:0.8.810/", "12 ms"));
+        peersData.add(new PeersModel(2, "192.168.0.2:8080", "/Apis Core:0.8.810/", "65548 ms"));
+        peersData.add(new PeersModel(3, "192.168.0.5:8080", "/Apis Core:0.8.810/", "102 ms"));
+        peersData.add(new PeersModel(4, "192.168.0.6:8080", "/Apis Core:0.8.810/", "250 ms"));
+        peersData.add(new PeersModel());
+        peersData.add(new PeersModel(1, "192.168.0.3:8080", "/Apis Core:0.8.810/", "12 ms"));
+        peersData.add(new PeersModel(2, "192.168.0.2:8080", "/Apis Core:0.8.810/", "65548 ms"));
+        peersData.add(new PeersModel(3, "192.168.0.5:8080", "/Apis Core:0.8.810/", "102 ms"));
+        peersData.add(new PeersModel(4, "192.168.0.6:8080", "/Apis Core:0.8.810/", "250 ms"));
     }
 
     public void initializeColumns() {
@@ -97,7 +124,7 @@ public class PopupPeersController extends BasePopupController {
         nodeIdCol.prefWidthProperty().bind(peersTable.widthProperty().divide(15).multiply(2));
         nodeServiceCol.prefWidthProperty().bind(peersTable.widthProperty().divide(15).multiply(5));
         userAgentCol.prefWidthProperty().bind(peersTable.widthProperty().divide(15).multiply(5));
-        pingCol.prefWidthProperty().bind(peersTable.widthProperty().divide(100).multiply(19));
+        pingCol.prefWidthProperty().bind(peersTable.widthProperty().divide(5).multiply(1));
     }
 
     private void customComparator() {
@@ -153,6 +180,64 @@ public class PopupPeersController extends BasePopupController {
         });
     }
 
+    private final EventHandler<MouseEvent> mouseEventHandler = new EventHandler<MouseEvent>() {
+        @Override
+        public void handle(MouseEvent event) {
+            String fxid = ((Node)event.getSource()).getId();
+
+            if(fxid.equals("peersTable")) {
+                if(peersTable.getSelectionModel().getSelectedIndex() >= 0) {
+                    setDetailList(peersTable.getSelectionModel().getSelectedIndex());
+                    setDetailVisible();
+                }
+            }
+        }
+    };
+
+    private void setDetailList(int index) {
+        // Exception handling for reordering list
+        ObservableList<PeersModel> currentPeersData = peersTable.getItems();
+        PeersModel model = currentPeersData.get(index);
+
+        selectedNode.setText(model.getNodeService() + " (node id: " + model.getNodeId() + ")");
+        viaNode.setText("via " + "model.getViaNodeService");
+
+        // Add Details
+        peersDetailVBox.getChildren().clear();
+        addDetailItem("WhiteListed", "No");
+        addDetailItem("Direction", "Outbound");
+        addDetailItem("Version", "70208");
+        addDetailItem("User Agent", model.getUserAgent());
+        addDetailItem("Services", "NETWORK & BLOOM");
+        addDetailItem("Starting Block", "774365");
+        addDetailItem("Synced Headers", "774365");
+        addDetailItem("Synced Blocks", "774365");
+        addDetailItem("Ban Score", "0");
+        addDetailItem("Connection Time", "57 s");
+        addDetailItem("Last Send", "0 s");
+        addDetailItem("Last Receive", "0 s");
+        addDetailItem("Sent", "77 KB");
+        addDetailItem("Received", "137 KB");
+        addDetailItem("Ping Time", model.getPing());
+        addDetailItem("Ping Wait", "N/A");
+        addDetailItem("Min Ping", model.getPing());
+        addDetailItem("Time Offset", "-3 s");
+    }
+
+    private void addDetailItem(String head, String contents) {
+        try {
+            BaseFxmlController fxmlController = new BaseFxmlController("popup/popup_peers_detail_item.fxml");
+            PopupPeersDetailItemController itemController = (PopupPeersDetailItemController) fxmlController.getController();
+
+            itemController.setHeadText(head);
+            itemController.setContentsText(contents);
+
+            peersDetailVBox.getChildren().add(fxmlController.getNode());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
     public void setDefaultVisible() {
         detailGrid.setVisible(false);
         defaultGrid.setVisible(true);
@@ -167,35 +252,4 @@ public class PopupPeersController extends BasePopupController {
         return peersData;
     }
 
-    class TableColumnComparator<S> implements Comparator<S> {
-        private final List<TableColumn<S, ?>> allColumns;
-
-        public TableColumnComparator(final List<TableColumn<S, ?>> allColumns) {
-            this.allColumns = allColumns;
-        }
-
-        @Override
-        public int compare(S o1, S o2) {
-            for(final TableColumn<S, ?> tc : allColumns) {
-                if(!isSortable(tc)) {
-                    continue;
-                }
-                final Object value1 = tc.getCellData(o1);
-                final Object value2 = tc.getCellData(o2);
-
-                @SuppressWarnings("unchecked") final Comparator<Object> c = (Comparator<Object>) tc.getComparator();
-                final int result = ASCENDING.equals(tc.getSortType()) ? c.compare(value1, value2)
-                        : c.compare(value2, value1);
-
-                if(result != 0) {
-                    return result;
-                }
-            }
-            return 0;
-        }
-
-        private boolean isSortable(final TableColumn<S, ?> tc) {
-            return tc.getSortType() != null && tc.isSortable();
-        }
-    }
 }
